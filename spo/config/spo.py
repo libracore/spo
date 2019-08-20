e46799004ef8ff79da675569fa501de5c149602e
Joelios <joel.mesmer@libracore.com>
2019-08-20 16:11:03 +0200

    new dt Mitgliedschaft and some stuff for modul page

--- spo/config/spo.py
+++ spo/config/spo.py
@@ -10,12 +10,50 @@ def get_data():
 				{
 					"type": "doctype",
 					"name": "Anfrage",
+					"label": "Anfragen",
 					"description": _("Mandats und oder sonstige Anfragen"),
 				},
 				{
 					"type": "doctype",
 					"name": "Mandat",
+					"label": "Mandate",
 					"description": _("Führendes Mandats-Dokument"),
+				}
+			]
+		},
+		{
+			"label": _("Mitgliederverwaltung"),
+			"icon": "fa fa-cog",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Mitgliedschaft",
+					"label": "Mitgliedschaften",
+					"description": _("SPO Mitgliedschaften"),
+				},
+				{
+					"type": "doctype",
+					"name": "Customer",
+					"label": "Kunden",
+					"description": _("Customers"),
+				},
+				{
+					"type": "doctype",
+					"label": "Rechnungen",
+					"name": "Sales Invoice",
+					"description": _("Sales Invoice"),
+				}
+			]
+		},
+		{
+			"label": _("Protokolle & Checklisten"),
+			"icon": "fa fa-cog",
+			"items": [
+				{
+					"type": "doctype",
+					"name": "Anfrage",
+					"label": "Platzhalter",
+					"description": _("Mandats und oder sonstige Anfragen"),
 				}
 			]
 		}
