d01e6323f4322c5bd4c3d2d39ae96e95a156e831
Joelios <joel.mesmer@libracore.com>
2020-03-16 12:27:38 +0100

    ready for implementation

--- spo/spo/doctype/mandat/mandat_dashboard.py
+++ spo/spo/doctype/mandat/mandat_dashboard.py
@@ -1,20 +1,20 @@
 from frappe import _
 
 def get_data():
-   return {
-      'fieldname': 'mandat',
-      'transactions': [
-         {
-            'label': _('Anforderungen'),
-            'items': ['Vollmacht', 'Anforderung Patientendossier', 'Medizinischer Bericht', 'Triage', 'Abschlussbericht']
-         },
-         {
-            'label': _('Buchhaltung'),
-            'items': ['Sales Invoice', 'Purchase Invoice']
-         },
-         {
-            'label': _('Diverses'),
-            'items': ['Freies Schreiben']
-         }
-      ]
-   }
+	return {
+		'fieldname': 'mandat',
+		'transactions': [
+			{
+				'label': _('Anforderungen'),
+				'items': ['Vollmacht', 'Anforderung Patientendossier', 'Medizinischer Bericht', 'Triage', 'Abschlussbericht']
+			},
+			{
+				'label': _('Buchhaltung'),
+				'items': ['Sales Invoice', 'Purchase Invoice']
+			},
+			{
+				'label': _('Diverses'),
+				'items': ['Freies Schreiben', 'SPO Anhang']
+			}
+		]
+	}
