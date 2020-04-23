259bb0573b0d61c1d5141eb6af3a947e341924eb
Joelios <joel.mesmer@libracore.com>
2020-04-23 11:15:23 +0200

    fix of ISS-00166

--- spo/spo/doctype/medizinischer_bericht/medizinischer_bericht.py
+++ spo/spo/doctype/medizinischer_bericht/medizinischer_bericht.py
@@ -21,7 +21,7 @@ def get_deckblat_data(mandat):
 		else:
 			data["name_klient"] = ''
 			data["geburtsdatum_klient"] = ''
-		employee = frappe.db.sql("""SELECT `name` FROM `tabEmployee` WHERE `user_id` = '{owner}'""".format(owner=mandat.owner), as_dict=True)
+		employee = frappe.db.sql("""SELECT `name` FROM `tabEmployee` WHERE `user_id` = '{owner}'""".format(owner=frappe.session.user), as_dict=True)
 		if len(employee) > 0:
 			data["beraterin"] = employee[0].name
 		else:
