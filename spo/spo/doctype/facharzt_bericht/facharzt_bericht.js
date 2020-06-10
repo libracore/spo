36c2337874ed2bf8087e8081f620d628470636c5
Joelios <joel.mesmer@libracore.com>
2020-06-10 09:02:22 +0200

    facharztbericht

--- spo/spo/doctype/facharzt_bericht/facharzt_bericht.js
+++ spo/spo/doctype/facharzt_bericht/facharzt_bericht.js
@@ -2,7 +2,53 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Facharzt Bericht', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		set_filters(frm);
+		if (!cur_frm.doc.adressat) {
+			set_adressat(frm);
+		}
+	},
+	facharzt_kontakt: function(frm) {
+		set_adressat(frm);
+	},
+	facharzt_adresse: function(frm) {
+		set_adressat(frm);
+	}
 });
+
+function set_filters(frm) {
+	cur_frm.fields_dict['facharzt_kontakt'].get_query = function(doc) {
+		return {
+			filters: {
+				"link_doctype": "Customer",
+				"link_name": cur_frm.doc.facharzt
+			}
+		}
+	}
+	cur_frm.fields_dict['facharzt_adresse'].get_query = function(doc) {
+		return {
+			filters: {
+				"link_doctype": "Customer",
+				"link_name": cur_frm.doc.facharzt
+			}
+		}
+	}
+}
+
+function set_adressat(frm) {
+	frappe.call({
+		"method": "spo.spo.doctype.facharzt_bericht.facharzt_bericht.get_adressat",
+		"args": {
+			"facharzt": cur_frm.doc.facharzt,
+			"facharzt_name": cur_frm.doc.facharzt_name,
+			"kontakt": cur_frm.doc.facharzt_kontakt,
+			"adresse": cur_frm.doc.facharzt_adresse
+		},
+		"async": false,
+		"callback": function(r) {
+			if (r.message) {
+				cur_frm.set_value('adressat', r.message);
+			}
+		}
+	});
+}
--- spo/spo/doctype/facharzt_bericht/facharzt_bericht.py
+++ spo/spo/doctype/facharzt_bericht/facharzt_bericht.py
@@ -3,8 +3,42 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class FacharztBericht(Document):
 	pass
+
+@frappe.whitelist()
+def get_adressat(facharzt=None, facharzt_name=None, kontakt=None, adresse=None):
+	if facharzt:
+		if kontakt:
+			kontakt = frappe.get_doc("Contact", kontakt)
+		else:
+			try:
+				_kontakt = frappe.get_all('Dynamic Link', filters={'link_doctype': 'Customer', 'link_name': facharzt, 'parenttype': 'Contact'}, fields=['parent'])[0].parent
+				kontakt = frappe.get_doc("Contact", _kontakt)
+			except:
+				kontakt = None
+		if adresse:
+			adresse = frappe.get_doc("Address", adresse)
+		else:
+			try:
+				_adresse = frappe.get_all('Dynamic Link', filters={'link_doctype': 'Customer', 'link_name': facharzt, 'parenttype': 'Address'}, fields=['parent'])[0].parent
+				adresse = frappe.get_doc("Address", _adresse)
+			except:
+				adresse = None
+				
+		adressat = ''
+		if kontakt:
+			adressat += kontakt.salutation + "\n"
+			adressat += kontakt.first_name + " " + kontakt.last_name + "\n"
+		else:
+			adressat += facharzt_name + "\n"
+		if adresse:
+			adressat += adresse.address_line1 + "\n"
+			if adresse.address_line2:
+				adressat += adresse.address_line2 + "\n"
+			adressat += adresse.plz + " " + adresse.city + "\n"
+		return adressat
+	return False
