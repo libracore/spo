5f03dadbe9ef9dd1de2d0cbce8267dc355810f23
Joelios <joel.mesmer@libracore.com>
2020-06-11 10:13:48 +0200

    rechnungsformular

--- spo/spo/doctype/facharzt_bericht/facharzt_bericht.js
+++ spo/spo/doctype/facharzt_bericht/facharzt_bericht.js
@@ -5,14 +5,23 @@ frappe.ui.form.on('Facharzt Bericht', {
 	refresh: function(frm) {
 		set_filters(frm);
 		if (!cur_frm.doc.adressat) {
-			set_adressat(frm);
+			set_facharzt_adressat(frm);
+		}
+		if (!cur_frm.doc.patienten_anschrift) {
+			set_patient_adressat(frm);
 		}
 	},
 	facharzt_kontakt: function(frm) {
-		set_adressat(frm);
+		set_facharzt_adressat(frm);
 	},
 	facharzt_adresse: function(frm) {
-		set_adressat(frm);
+		set_facharzt_adressat(frm);
+	},
+	patienten_kontakt: function(frm) {
+		set_patient_adressat(frm);
+	},
+	patienten_adresse: function(frm) {
+		set_patient_adressat(frm);
 	}
 });
 
@@ -33,9 +42,25 @@ function set_filters(frm) {
 			}
 		}
 	}
+	cur_frm.fields_dict['patienten_kontakt'].get_query = function(doc) {
+		return {
+			filters: {
+				"link_doctype": "Customer",
+				"link_name": cur_frm.doc.patient
+			}
+		}
+	}
+	cur_frm.fields_dict['patienten_adresse'].get_query = function(doc) {
+		return {
+			filters: {
+				"link_doctype": "Customer",
+				"link_name": cur_frm.doc.patient
+			}
+		}
+	}
 }
 
-function set_adressat(frm) {
+function set_facharzt_adressat(frm) {
 	frappe.call({
 		"method": "spo.spo.doctype.facharzt_bericht.facharzt_bericht.get_adressat",
 		"args": {
@@ -48,6 +73,26 @@ function set_adressat(frm) {
 		"callback": function(r) {
 			if (r.message) {
 				cur_frm.set_value('adressat', r.message);
+			}
+		}
+	});
+}
+
+function set_patient_adressat(frm) {
+	console.log("ok");
+	frappe.call({
+		"method": "spo.spo.doctype.facharzt_bericht.facharzt_bericht.get_adressat",
+		"args": {
+			"facharzt": cur_frm.doc.patient,
+			"facharzt_name": 'Bitte Vor- und Nachnamen eintragen',
+			"kontakt": cur_frm.doc.patienten_kontakt,
+			"adresse": cur_frm.doc.patienten_adresse
+		},
+		"async": false,
+		"callback": function(r) {
+			console.log(r.message);
+			if (r.message) {
+				cur_frm.set_value('patienten_anschrift', r.message);
 			}
 		}
 	});
--- spo/spo/doctype/facharzt_bericht/facharzt_bericht.py
+++ spo/spo/doctype/facharzt_bericht/facharzt_bericht.py
@@ -11,34 +11,43 @@ class FacharztBericht(Document):
 
 @frappe.whitelist()
 def get_adressat(facharzt=None, facharzt_name=None, kontakt=None, adresse=None):
-	if facharzt:
-		if kontakt:
-			kontakt = frappe.get_doc("Contact", kontakt)
-		else:
-			try:
-				_kontakt = frappe.get_all('Dynamic Link', filters={'link_doctype': 'Customer', 'link_name': facharzt, 'parenttype': 'Contact'}, fields=['parent'])[0].parent
-				kontakt = frappe.get_doc("Contact", _kontakt)
-			except:
-				kontakt = None
-		if adresse:
-			adresse = frappe.get_doc("Address", adresse)
-		else:
-			try:
-				_adresse = frappe.get_all('Dynamic Link', filters={'link_doctype': 'Customer', 'link_name': facharzt, 'parenttype': 'Address'}, fields=['parent'])[0].parent
-				adresse = frappe.get_doc("Address", _adresse)
-			except:
-				adresse = None
-				
-		adressat = ''
-		if kontakt:
-			adressat += kontakt.salutation + "\n"
-			adressat += kontakt.first_name + " " + kontakt.last_name + "\n"
-		else:
-			adressat += facharzt_name + "\n"
-		if adresse:
-			adressat += adresse.address_line1 + "\n"
-			if adresse.address_line2:
-				adressat += adresse.address_line2 + "\n"
-			adressat += adresse.plz + " " + adresse.city + "\n"
-		return adressat
-	return False
+	try:
+		if facharzt:
+			if kontakt:
+				kontakt = frappe.get_doc("Contact", kontakt)
+			else:
+				try:
+					_kontakt = frappe.get_all('Dynamic Link', filters={'link_doctype': 'Customer', 'link_name': facharzt, 'parenttype': 'Contact'}, fields=['parent'])[0].parent
+					kontakt = frappe.get_doc("Contact", _kontakt)
+				except:
+					kontakt = None
+			if adresse:
+				adresse = frappe.get_doc("Address", adresse)
+			else:
+				try:
+					_adresse = frappe.get_all('Dynamic Link', filters={'link_doctype': 'Customer', 'link_name': facharzt, 'parenttype': 'Address'}, fields=['parent'])[0].parent
+					adresse = frappe.get_doc("Address", _adresse)
+				except:
+					adresse = None
+					
+			adressat = ''
+			if kontakt:
+				if kontakt.salutation:
+					adressat += kontakt.salutation + "\n"
+				if kontakt.first_name and kontakt.last_name:
+					adressat += kontakt.first_name + " " + kontakt.last_name + "\n"
+				else:
+					adressat += facharzt_name + "\n"
+			else:
+				adressat += facharzt_name + "\n"
+			if adresse:
+				if adresse.address_line1:
+					adressat += adresse.address_line1 + "\n"
+				if adresse.address_line2:
+					adressat += adresse.address_line2 + "\n"
+				if adresse.plz and adresse.city:
+					adressat += adresse.plz + " " + adresse.city + "\n"
+			return adressat
+		return False
+	except Exception as e:
+		frappe.throw(e)
