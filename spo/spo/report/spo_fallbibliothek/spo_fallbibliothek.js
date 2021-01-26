7edb68ff9a963f820cb524fb362a3a5504de0296
Joelios <joel.mesmer@libracore.com>
2021-01-26 14:01:22 +0100

    TASK00499

--- spo/spo/report/spo_fallbibliothek/spo_fallbibliothek.js
+++ spo/spo/report/spo_fallbibliothek/spo_fallbibliothek.js
@@ -5,5 +5,19 @@
 frappe.query_reports["SPO Fallbibliothek"] = {
 	"filters": [
 
-	]
+	],
+    get_datatable_options(options) {
+		delete options['cellHeight']
+		// change datatable options
+		return Object.assign(options, {
+			dynamicRowHeight: true
+		});
+	},
+	onload: function(report) {
+		report.page.add_inner_button(__("Extend Row"), function() {
+			$(".dt-row.vrow").each(function(){
+                $(this).addClass("extend-row");
+            });
+		});
+	}
 };
