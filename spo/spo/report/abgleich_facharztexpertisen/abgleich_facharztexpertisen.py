35fc0ed366862fe9969090b9d343d934f39209e3
Joelios <joel.mesmer@libracore.com>
2021-04-21 12:16:22 +0200

    TASK00555

--- spo/spo/report/abgleich_facharztexpertisen/abgleich_facharztexpertisen.py
+++ spo/spo/report/abgleich_facharztexpertisen/abgleich_facharztexpertisen.py
@@ -9,8 +9,10 @@ from frappe.utils.data import getdate
 def execute(filters=None):
     columns, data = [
         {"label": _("Mandat"), "fieldname": "mandat", "fieldtype": "Link", "options": "Mandat"},
+        {"label": _("PINV"), "fieldname": "pinv_name", "fieldtype": "Link", "options": "Purchase Invoice"},
         {"label": _("Eingangsrechnung (in CHF)"), "fieldname": "pinv", "fieldtype": "Float"},
         {"label": _("Datum"), "fieldname": "pinv_date", "fieldtype": "Date", "width": "100"},
+        {"label": _("SINV"), "fieldname": "sinv_name", "fieldtype": "Link", "options": "Sales Invoice"},
         {"label": _("Ausgangsrechnung (in CHF)"), "fieldname": "sinv", "fieldtype": "Float"},
         {"label": _("Datum"), "fieldname": "sinv_date", "fieldtype": "Date", "width": "100"},
         {"label": _("Rechnungsstatus"), "fieldname": "rechnungsstatus", "fieldtype": "Data", "width": "100"},
@@ -26,27 +28,30 @@ def execute(filters=None):
         _data.append(mandat[0])
         pinv_amount = 0
         sinv_amount = 0
-        pinv = frappe.db.sql("""SELECT `grand_total`, `posting_date` FROM `tabPurchase Invoice` WHERE `mandat` = '{mandat}' AND `docstatus` != '2' AND `posting_date` >= '{abgleich_ab}' LIMIT 1""".format(mandat=mandat[0], abgleich_ab=abgleich_ab), as_dict=True)
+        pinv = frappe.db.sql("""SELECT `grand_total`, `posting_date`, `name` FROM `tabPurchase Invoice` WHERE `mandat` = '{mandat}' AND `docstatus` != '2' AND `posting_date` >= '{abgleich_ab}' LIMIT 1""".format(mandat=mandat[0], abgleich_ab=abgleich_ab), as_dict=True)
         if len(pinv) > 0:
+            _data.append(pinv[0].name)
             _data.append(pinv[0].grand_total)
             _data.append(pinv[0].posting_date)
             if pinv[0].grand_total:
                 pinv_amount = float(pinv[0].grand_total)
         else:
+            _data.append("")
             _data.append(0.00)
             _data.append("")
         sinv_query = """SELECT `name` FROM `tabSales Invoice` WHERE `mandat` = '{mandat}' AND `docstatus` != '2' AND `posting_date` >= '{abgleich_ab}'""".format(mandat=mandat[0], abgleich_ab=abgleich_ab)
-        sinv_date = frappe.db.sql("""SELECT `posting_date`, `status` FROM `tabSales Invoice` WHERE `mandat` = '{mandat}' AND `docstatus` != '2' AND `posting_date` >= '{abgleich_ab}' LIMIT 1""".format(mandat=mandat[0], abgleich_ab=abgleich_ab), as_list=True)
+        sinv_date = frappe.db.sql("""SELECT `posting_date`, `status`, `name` FROM `tabSales Invoice` WHERE `mandat` = '{mandat}' AND `docstatus` != '2' AND `posting_date` >= '{abgleich_ab}' LIMIT 1""".format(mandat=mandat[0], abgleich_ab=abgleich_ab), as_dict=True)
         if len(sinv_date) > 0:
+            _data.append(sinv_date[0].name)
             sinv = frappe.db.sql("""SELECT SUM(`amount`) FROM `tabSales Invoice Item` WHERE `parent` IN ({sinv_query}) AND `item_code` = 'Mandatsverrechnung (exkl. MwSt)'""".format(sinv_query=sinv_query), as_list=True)
             if sinv[0][0]:
                 sinv_amount = float(sinv[0][0])
                 _data.append(sinv[0][0])
             else:
                 _data.append(0.00)
-            _data.append(sinv_date[0][0])
-            _data.append(sinv_date[0][1])
-            if sinv_date[0][1] == 'Paid':
+            _data.append(sinv_date[0].posting_date)
+            _data.append(sinv_date[0].status)
+            if sinv_date[0].status == 'Paid':
                 payment_date = frappe.db.sql("""SELECT `modified` FROM `tabPayment Entry Reference` WHERE `parentfield` = 'references' AND `parenttype` = 'Payment Entry' AND `reference_name` IN ({sinv_query}) LIMIT 1""".format(sinv_query=sinv_query), as_list=True)
                 if len(payment_date) > 0:
                     _data.append(getdate(payment_date[0][0]))
@@ -55,6 +60,7 @@ def execute(filters=None):
             else:
                 _data.append("")
         else:
+            _data.append("")
             _data.append(0.00)
             _data.append("")
             _data.append("")
