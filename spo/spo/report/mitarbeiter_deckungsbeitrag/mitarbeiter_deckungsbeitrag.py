b997f4baa543173f23d365a1e281fbf4de30705a
Joelios <joel.mesmer@libracore.com>
2021-04-21 14:00:51 +0200

    TASK00556

--- spo/spo/report/mitarbeiter_deckungsbeitrag/mitarbeiter_deckungsbeitrag.py
+++ spo/spo/report/mitarbeiter_deckungsbeitrag/mitarbeiter_deckungsbeitrag.py
@@ -11,7 +11,12 @@ def execute(filters=None):
         {"label": _("Name"), "fieldname": "employee_name", "fieldtype": "Data"},
         {"label": _("Arbeitszeit"), "fieldname": "arbeitszeit", "fieldtype": "Float"},
         {"label": _("Verrechenbar"), "fieldname": "verrechenbar", "fieldtype": "Float"},
+        {"label": _("Markiert als NV"), "fieldname": "markiert_als_nv", "fieldtype": "Float"},
+        {"label": _("NV in %"), "fieldname": "nv_in_prozent", "fieldtype": "Data"},
         {"label": _("Verrechnet"), "fieldname": "verrechnet", "fieldtype": "Float"},
+        {"label": _("Verrechnet via Mitgliedschaft"), "fieldname": "verrechnet_via_mitgliedschaft", "fieldtype": "Float"},
+        {"label": _("Differenz Kontrolle"), "fieldname": "differenz_kontrolle", "fieldtype": "Float"},
+        {"label": _("Differenz Kontrolle in %"), "fieldname": "differenz_kontrolle_in_prozent", "fieldtype": "Data"},
         {"label": _("DB Total in %"), "fieldname": "db_total", "fieldtype": "Data"},
         {"label": _("DB Verrechenbar in %"), "fieldname": "db_verrechenbar", "fieldtype": "Data"}
         ], []
@@ -39,13 +44,42 @@ def execute(filters=None):
             verrechenbar = 0.00
         _data.append(verrechenbar)
         
+        _markiert_als_nv = frappe.db.sql("""SELECT SUM(`hours`) FROM `tabTimesheet Detail` WHERE `parent` IN ({timesheets_query}) AND `activity_type` IN ('Beratung', 'Mandatsarbeit') AND `nicht_verrechnen` = 1""".format(timesheets_query=timesheets_query), as_list=True)
+        if _markiert_als_nv[0][0]:
+            markiert_als_nv = _markiert_als_nv[0][0]
+        else:
+            markiert_als_nv = 0.00
+        _data.append(markiert_als_nv)
+        
+        nv_in_prozent = 0.00
+        if verrechenbar > 0:
+            nv_in_prozent = (100 / verrechenbar) * markiert_als_nv
+        _data.append(round(nv_in_prozent, 2))
+        
+        # nicht verrechnete zeiten aus anfragen ohne mandat aber it mitgliedschaft
+        anf_ausschluss_query = """SELECT `anfragen` FROM `tabMandat`"""
+        anf_query = """SELECT `name` FROM `tabAnfrage` WHERE `mitgliedschaft` IS NOT NULL AND `name` NOT IN ({anf_ausschluss_query})""".format(anf_ausschluss_query=anf_ausschluss_query)
+        _verrechnet_zusatz = frappe.db.sql("""SELECT SUM(`hours`) FROM `tabTimesheet Detail` WHERE `parent` IN ({timesheets_query}) AND `activity_type` = 'Beratung' AND `spo_referenz` IN ({anf_query})""".format(timesheets_query=timesheets_query, anf_query=anf_query), as_list=True)
+        if _verrechnet_zusatz[0][0]:
+            verrechnet_zusatz = _verrechnet_zusatz[0][0]
+        else:
+            verrechnet_zusatz = 0.00
+        
         sinv_query = """SELECT `name` FROM `tabSales Invoice` WHERE `docstatus` != '2' AND `company` = 'SPO Schweizerische Patientenorganisation' AND `posting_date` BETWEEN '{start}' AND '{ende}'""".format(start=start, ende=ende)
         _verrechnet = frappe.db.sql("""SELECT SUM(`qty`) FROM `tabSales Invoice Item` WHERE `parent` IN ({sinv_query}) AND `employee` = '{employee}'""".format(sinv_query=sinv_query, employee=employee.name), as_list=True)
         if _verrechnet[0][0]:
-            verrechnet = _verrechnet[0][0]
+            verrechnet = _verrechnet[0][0] + verrechnet_zusatz
         else:
-            verrechnet = 0.00
+            verrechnet = 0.00 + verrechnet_zusatz
         _data.append(verrechnet)
+        _data.append(verrechnet_zusatz)
+        
+        differenz_kontrolle = verrechenbar - verrechnet - markiert_als_nv
+        _data.append(differenz_kontrolle)
+        differenz_kontrolle_in_prozent = 0.00
+        if verrechenbar > 0:
+            differenz_kontrolle_in_prozent = (100 / verrechenbar) * differenz_kontrolle
+        _data.append(round(differenz_kontrolle_in_prozent, 2))
         
         if arbeitszeit > 0:
             db_total = (100 / arbeitszeit) * verrechnet
