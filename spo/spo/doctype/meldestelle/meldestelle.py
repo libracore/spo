6aa8b5a88def2551b564488b739fb841a5cea7e9
joelios <joel.mesmer@libracore.com>
2021-08-26 00:31:51 +0200

    verify reCaptcha

--- spo/spo/doctype/meldestelle/meldestelle.py
+++ spo/spo/doctype/meldestelle/meldestelle.py
@@ -5,6 +5,7 @@
 from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
+import requests
 
 class Meldestelle(Document):
 	pass
@@ -13,17 +14,22 @@ class Meldestelle(Document):
 @frappe.whitelist(allow_guest=True)
 def new_request():
     try:
-        data = frappe.local.form_dict
-        nr = frappe.get_doc({
-            "doctype": "Meldestelle",
-            "mandant": data.mandant,
-            "report_from": data.fname + " " + data.lname,
-            "phone": data.phone,
-            "email": data.email,
-            "availability": data.availability,
-            "report": data.report
-        })
-        nr.insert(ignore_permissions=True)
-        return True
+        data = frappe.local.form_dict    
+        payload = {'secret': '6LfJyw4cAAAAAMARDEfjxiBke-8pQIWbmDnmnFoi', 'response': data['g-recaptcha']}
+        r = requests.get("https://www.google.com/recaptcha/api/siteverify", params=payload)
+        if r.success:
+            nr = frappe.get_doc({
+                "doctype": "Meldestelle",
+                "mandant": data.mandant,
+                "report_from": data.fname + " " + data.lname,
+                "phone": data.phone,
+                "email": data.email,
+                "availability": data.availability,
+                "report": data.report
+            })
+            nr.insert(ignore_permissions=True)
+            return True
+        else:
+            return False
     except:
         return False
