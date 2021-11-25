696286ea1c48d8021c33ce4b368883101bec1dca
Joelios <joel.mesmer@libracore.com>
2021-11-25 16:32:32 +0100

    doctypes

---
+++ spo/spo/doctype/beratungsslot/beratungsslot.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright (c) 2021, libracore and contributors
+# For license information, please see license.txt
+
+from __future__ import unicode_literals
+# import frappe
+from frappe.model.document import Document
+
+class Beratungsslot(Document):
+	pass
+
+@frappe.whitelist()
+def get_slots():
