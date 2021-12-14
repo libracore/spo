d0e7fb06e94578ec5fbd747d7cdab2e0a375765e
nanipi <nicole.gret@libracore.com>
2021-12-14 14:58:18 +0100

    adjusted doctype sloteingabe and details (childtbl), new doctypes beraterzuweisung prepared for later, currently work with select. custom script on sloteingabe still static, prepared dialog, sat sun still create lines for slots, slot creation outstanding

--- spo/spo/doctype/beraterzuweisung/beraterzuweisung.js
+++ spo/spo/doctype/beraterzuweisung/beraterzuweisung.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2021, libracore and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Beratungsthema', {
+frappe.ui.form.on('Beraterzuweisung', {
 	// refresh: function(frm) {
 
 	// }
