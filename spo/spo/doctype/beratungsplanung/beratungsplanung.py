d87a542019e003c2991c2a999d6e16e4a769be24
lasalesi <larsericmueller@gmail.com>
2021-12-27 12:29:27 +0100

    added automatic slot generation

--- spo/spo/doctype/beratungsplanung/beratungsplanung.py
+++ spo/spo/doctype/beratungsplanung/beratungsplanung.py
@@ -2,12 +2,28 @@
 # Copyright (c) 2021, libracore and contributors
 # For license information, please see license.txt
 
-# from __future__ import unicode_literals
-# import frappe
-# from frappe.model.document import Document
+from __future__ import unicode_literals
+import frappe
+from frappe.model.document import Document
+from datetime import datetime
 
-# class Beratungsplanung(Document):
-    # on_submit(self):
-        # # TODO: create slots from child table
-        # print("create slots from child table here")
-        # return
+class Beratungsplanung(Document):
+    on_submit(self):
+        # create slots from child table
+        for slot in self.sloteingaben:
+            create_slot(date=slot.date , hour=slot.time, subject=slot.objective, consultant=slot.user)
+            
+        return
+
+
+def create_slot(date, hour, subject, consultant):
+    new_slot = frappe.get_doc({
+        'doctype': 'Beratungsslot',
+        'start': datetime.strptime("{0} {1}:00".format(date, hour), "%Y-%m-%d %H:%M"),
+        'end': datetime.strptime("{0} {1}:30".format(date, hour), "%Y-%m-%d %H:%M"),
+        'topic': subject,
+        'user': user,
+        'status': 'frei'
+    })
+    new_slot.insert()
+    return
