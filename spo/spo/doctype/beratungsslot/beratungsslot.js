d50aba382ccef45f8ca9901a2e8255c6033b8e7a
lasalesi <larsericmueller@gmail.com>
2022-01-13 15:07:33 +0100

    create invoice from slot

--- spo/spo/doctype/beratungsslot/beratungsslot.js
+++ spo/spo/doctype/beratungsslot/beratungsslot.js
@@ -14,6 +14,32 @@ frappe.ui.form.on('Beratungsslot', {
                     }
                 });
             });
+        } else {
+            if ((frm.doc.customer) && (!frm.doc.__islocal)) {
+                frm.add_custom_button(__("Rechnung erstellen"), function() {
+                    frappe.call({
+                        'method': 'spo.utils.onlinetermin.submit_request',
+                        'args': {
+                            'slot': frm.doc.name, 
+                            'member': frm.doc.customer, 
+                            'first_name': frm.doc.first_name || "", 
+                            'last_name': frm.doc.last_name || "", 
+                            'address': frm.doc.address || "", 
+                            'city': frm.doc.city || "", 
+                            'pincode': frm.doc.pincode || "", 
+                            'email': frm.doc.email_id || "", 
+                            'phone': frm.doc.phone || "",
+                            'geburtsdatum': frm.doc.geburtsdatum || "",
+                            'salutation_title': frm.doc.salutation_title || "",
+                        },
+                        'callback': function(response) {
+                            // invoice created
+                            cur_frm.doc.reload_doc();
+                            cur_frm.refresh();
+                        }
+                    });
+                });
+            }
         }
         // filter for berater based on topic
         cur_frm.fields_dict['user'].get_query = function(doc) {          
