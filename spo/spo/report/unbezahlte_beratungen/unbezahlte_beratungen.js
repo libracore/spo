676fb3b0b9ab22a4e99b4920157f8c98b85b8c05
Valentina <vjss05@gmail.com>
2022-07-25 11:24:41 +0200

    report apps

---
+++ spo/spo/report/unbezahlte_beratungen/unbezahlte_beratungen.js
@@ -0,0 +1,22 @@
+// Copyright (c) 2022, libracore and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Unbezahlte Beratungen"] = {
+	"filters": [
+        {
+            "fieldname":"from_date",
+            "label": __("From Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.add_months(frappe.datetime.get_today(), - 1 ),
+            "width": "60px"
+        },
+        {
+            "fieldname":"to_date",
+            "label": __("To Date"),
+            "fieldtype": "Date",
+            "default": frappe.datetime.get_today(),
+            "width": "60px"
+        },
+	]
+};
---
+++ spo/spo/report/unbezahlte_beratungen/unbezahlte_beratungen.py
@@ -0,0 +1,58 @@
+# Copyright (c) 2022, libracore and contributors
+# For license information, please see license.txt
+
+
+from __future__ import unicode_literals
+import frappe
+from frappe import _
+import ast
+
+def execute(filters=None):
+    columns = get_columns()
+    data = get_data(filters)
+    return columns, data
+
+def get_columns():
+    return [
+        {"label": _("Start"), "fieldname": "startdate", "fieldtype": "Time", "width": 160},
+        {"label": _("Slot"), "fieldname": "slot", "fieldtype": "Link", "options": "Beratungsslot", "width": 150},
+        {"label": _("Thema"), "fieldname": "thema", "fieldtype": "Data", "width": 100},
+        {"label": _("Art"), "fieldname": "art", "fieldtype": "Data", "width": 140},
+        {"label": _("Berater"), "fieldname": "berater", "fieldtype": "Data", "width": 140},
+        {"label": _("Kunde"), "fieldname": "kunde", "fieldtype": "Link", "options": "Customer", "width": 150},
+        {"label": _("Ombudsstellenpartner"), "fieldname": "partner", "fieldtype": "Data", "width": 180},
+        {"label": _("Satus"), "fieldname": "status", "fieldtype": "Data", "width": 70},
+    ]
+
+def get_data(filters):
+    if type(filters) is str:
+        filters = ast.literal_eval(filters)
+    else:
+        filters = dict(filters)
+    
+    conditions = ""
+    if 'from_date' in filters and filters['from_date']:
+        conditions += """ AND (`tabBeratungsslot`.`start` >= '{from_date}' OR `tabBeratungsslot`.`start` IS NULL)""".format(from_date=filters['from_date'])
+    if 'to_date' in filters and filters['to_date']:
+        conditions += """ AND (`tabBeratungsslot`.`start` <= '{to_date}' OR `tabBeratungsslot`.`start` IS NULL)""".format(to_date=filters['to_date'])
+        
+    # prepare query
+    sql_query = """
+        SELECT
+            `tabBeratungsslot`.`start` AS `startdate`,
+            `tabBeratungsslot`.`name` AS `slot`,
+            `tabBeratungsslot`.`topic` AS `thema`,
+            `tabBeratungsslot`.`consultation_type` AS `art`,
+            `tabBeratungsslot`.`user` AS `berater`,
+            `tabBeratungsslot`.`customer` AS `kunde`,
+            `tabBeratungsslot`.`ombudsstelle` AS `partner`,
+            `tabBeratungsslot`.`status` AS `status`
+        FROM `tabBeratungsslot`
+        WHERE `tabBeratungsslot`.`status` = "reserviert" 
+            {conditions}
+        ORDER BY `tabBeratungsslot`.`start` ASC
+      """.format(conditions=conditions)
+    #frappe.throw(sql_query)
+    data = frappe.db.sql(sql_query, as_dict=True)
+
+    return data
